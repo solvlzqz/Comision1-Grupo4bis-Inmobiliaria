0d7cf8d25c7e0f028e581fa62a122a8b4d46aeae
Sole <Sole@LAPTOP-G5ABMM0F>
2018-12-02 19:13:31 -0300

    proyecto de programadores3.0

---
+++ src/inmobiliaria/modelo/Conexion.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inmobiliaria.modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Sole
+ */
+
+public class Conexion {
+    
+    private String url="jdbc:mysql://localhost/inmobiliaria";
+    private String usuario="root";
+    private String password="";
+    private Connection con = null;
+
+    public Conexion() {
+        
+        try {
+            Class.forName ("org.mariadb.jdbc.Driver");
+        } catch ( ClassNotFoundException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+      } 
+    
+    public Connection getConexion() {
+        try {
+            if (con == null) {
+                con = DriverManager.getConnection(url+"?useLegacyDatetimeCode=false&serverTimezone=UTC"+
+                        "&user="+usuario+"&password="+password);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    return con;
+            }
+        }
